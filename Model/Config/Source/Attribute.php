951830fd839b2b168e1567fd03cdea06b325a52d
Marco Oliveira <marco.oliveira@monsoonconsulting.com>
2021-02-22 17:36:38 -0300

    Refatorando o modulo para a versão 2.0

---
+++ Model/Config/Source/Attribute.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @package     Intelipost_PreSales
+ * @copyright   Copyright (c) 2016 Gamuza Technologies (http://www.gamuza.com.br/)
+ * @author      Eneias Ramos de Melo <eneias@gamuza.com.br>
+ */
+
+namespace Intelipost\PreSales\Model\Config\Source;
+
+class Attribute extends \Intelipost\Quote\Model\Config\Source\Attribute
+{
+/*
+ * extends from Quote
+ */
+}
